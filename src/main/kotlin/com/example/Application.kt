044d517bc88d6d7083d023d45e040641eaca8e2e
Thomas Rye <thomasjrye@gmail.com>
2024-09-14 11:02:26 +0200

    initialize project

---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,20 @@
+// src/main/kotlin/com/example/Application.kt
+package com.example
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    routing {
+        get("/") {
+            call.respondText("Hello, World!")
+        }
+    }
+}
